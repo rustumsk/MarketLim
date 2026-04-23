171f48f67796cc0c3c3f98a5f8f01771d24e9785
rustumsk <rustumarellano@gmail.com>
2026-04-23 21:46:17 +0800

    added/ category model

---
+++ MarketLim/WebApplication1/Models/Category.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
